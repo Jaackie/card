e43a866dbea356ec5a8ff1780d395992f7edcc37
jaackie <jiaqingsir@foxmail.com>
2016-05-01 21:27:33 +0800

    git files

--- card.com/application/controllers/Index.php
+++ card.com/application/controllers/Index.php
@@ -6,7 +6,8 @@ class IndexController extends Yaf_Controller_Abstract
     {
         /*$redis = new Redis();
         $redis->connect('127.0.0.1',6379);*/
-        phpinfo();
-//        testModel::getInstance()->redis();
+//        phpinfo();
+        echo 'hello';
+        testModel::getInstance()->redis();
     }
 }
